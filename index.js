316e74dd54c27cbd252a7a87242277ac0b5243db
YoungjinNoh <youngjin_noh@naver.com>
2022-08-27 22:28:44 +0900

    api 서버 연동

--- index.js
+++ index.js
@@ -1,64 +1,92 @@
 let insertBtn = document.querySelector(".TodoInsert button");
 let todoList = document.querySelector(".TodoList")
-insertBtn.onclick = () => {
-    fetch("http://52.78.75.188:8080/todos").then((response) =>
-        console.log(response)
-    );
 
+function createTodo() {
     let todoString = document.querySelector(".TodoInsert input").value;
+
+
+
     if (todoString === "") {
         return;
     }
 
 
-    document.querySelector(".TodoInsert input").value = "";
-    let todoItem = document.createElement("div");
-    todoItem.className = "TodoListItem";
-    let checkBox = document.createElement("div");
-    checkBox.className = "checkbox";
-    let icon = document.createElement("div");
-    icon.className = "icon";
-    icon.appendChild(document.createTextNode("□"));
-    let text = document.createElement("div");
-    text.className = "text";
-    text.appendChild(document.createTextNode(todoString));
-    let button = document.createElement("button");
-    button.className = "remove";
-    button.appendChild(document.createTextNode("X"));
-
-    checkBox.onclick = () => {
-        checkBox.classList.toggle("checked");
-        if (checkBox.classList.contains("checked")) {
-            checkBox.querySelector(".icon").textContent = "■";
-        }
-        else {
-            checkBox.querySelector(".icon").textContent = "□";
-        }
-    }
-    button.onclick = () => { todoItem.remove() }
-
-
-
-    checkBox.appendChild(icon);
-    checkBox.appendChild(text);
-    todoItem.appendChild(checkBox);
-    todoItem.appendChild(button);
-    todoList.appendChild(todoItem);
-
+    fetch("http://52.78.75.188:8080/todos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            content: todoString,
+        }),
+    })
+        .then((response) => response.json())
+        .then(() => location.reload())
 }
 
-let todos = document.querySelectorAll(".TodoListItem")
-for (let i = 0; i < todos.length; i++) {
-    let checkbox = todos[i].querySelector(".checkbox");
-    let remove = todos[i].querySelector(".remove");
-    checkbox.onclick = () => {
-        checkbox.classList.toggle("checked");
-        if (checkbox.classList.contains("checked")) {
-            checkbox.querySelector(".icon").textContent = "■";
-        }
-        else {
-            checkbox.querySelector(".icon").textContent = "□";
-        }
-    }
-    remove.onclick = () => { todos[i].remove() }
+function deleteTodo(id) {
+    fetch("http://52.78.75.188:8080/todos/" + id, {
+        method: "DELETE",
+    })
+        .then((response) => response.json())
+        .then(() => location.reload())
 }
+
+function updateTodo(id) {
+    fetch("http://52.78.75.188:8080/todos/" + id, {
+        method: "PATCH",
+    })
+        .then((response) => response.json())
+        .then(() => location.reload())
+}
+
+function initTodos() {
+    fetch("http://52.78.75.188:8080/todos")
+        .then((response) => response.json())
+        .then((json) => {
+            json.forEach(todo => {
+                let todoItem = document.createElement("div");
+                todoItem.className = "TodoListItem";
+                let checkBox = document.createElement("div");
+                checkBox.className = "checkbox";
+                let icon = document.createElement("div");
+                icon.className = "icon";
+                if (todo.checked) {
+                    icon.appendChild(document.createTextNode("■"));
+                }
+                else {
+                    icon.appendChild(document.createTextNode("□"));
+                }
+                let text = document.createElement("div");
+                text.className = "text";
+                text.appendChild(document.createTextNode(todo.content));
+                let button = document.createElement("button");
+                button.className = "remove";
+                button.appendChild(document.createTextNode("X"));
+
+                checkBox.onclick = () => { updateTodo(todo.id) }
+                button.onclick = () => { deleteTodo(todo.id) }
+
+
+
+                checkBox.appendChild(icon);
+                checkBox.appendChild(text);
+                todoItem.appendChild(checkBox);
+                todoItem.appendChild(button);
+                todoList.appendChild(todoItem);
+            })
+
+        })
+}
+
+
+insertBtn.onclick = createTodo;
+
+initTodos();
+
+
+
+
+
+
+
